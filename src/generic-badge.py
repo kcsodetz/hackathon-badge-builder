0220ab4b6e8848ea0f6f99e10652b0cae6f9ddb6
kcsodetz <kensodetz@gmail.com>
2020-05-27 18:35:06 -0400

    Correct bm iteration

--- src/generic-badge.py
+++ src/generic-badge.py
@@ -1,5 +1,5 @@
 """
-Python script to generate the blank Hacker and Sponsor badges for BoilerMake VI
+Python script to generate the blank Hacker and Sponsor badges for BoilerMake VII
 
 Author: Ken Sodetz
 Since: 10/17/2018
